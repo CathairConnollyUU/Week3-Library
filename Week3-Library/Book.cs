87ba53b3f5dc9af3ee21b1411c9003d5eb01f1f0
Cathair Connolly <Connolly-C67@ulster.ac.uk>
2025-10-06 16:11:42 +0100

    Updated code with access modifiers

--- Week3-Library/Book.cs
+++ Week3-Library/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Week3_Library
 {
-    internal class Book
+    class Book
     {
         string Title;
         string Author;
@@ -21,29 +21,12 @@ namespace Week3_Library
             ISBN = bookISBN;
         }
 
-        void DisplayInfo()
+        public void DisplayInfo()
         {
             Console.WriteLine($"Book title: {Title}");
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
         }
-
-        static void Main(string[] args)
-        {
-            // Create a new instance (object) of the Book class
-            // Note how the object name differs from the class name
-            Book myBook1 = new Book("C# for beginners", "Bill Gates", "1234");
-
-            // This is another book object.
-            // Note that the object name is 'book1' and not 'book'
-            Book myBook2 = new Book("C# methods", "Microsoft", "3456778");
-
-            // Output book information to the console
-            myBook1.DisplayInfo();
-            myBook2.DisplayInfo();
-
-        }
-
     }
 }
--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Week3_Library;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance (object) of the Book class
+        // Note how the object name differs from the class name
+        Book myBook1 = new Book("C# for beginners", "Bill Gates", "1234");
+
+        // Output book information to the console
+        myBook1.DisplayInfo();
+    }
+}
